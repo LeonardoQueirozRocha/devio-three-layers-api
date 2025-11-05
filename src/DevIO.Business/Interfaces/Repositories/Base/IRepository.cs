ec4e7083c1dcfef6de05b904495fb24f3e432909
Leonardo Queiroz Rocha <47957548+LeonardoQueirozRocha@users.noreply.github.com>
2025-11-05 08:56:32 -0300

    feat: Implement base repository with common CRUD operations and query support

--- src/DevIO.Business/Interfaces/Repositories/Base/IRepository.cs
+++ src/DevIO.Business/Interfaces/Repositories/Base/IRepository.cs
@@ -5,17 +5,17 @@ namespace DevIO.Business.Interfaces.Repositories.Base;
 
 public interface IRepository<TEntity> : IDisposable where TEntity : Entity
 {
-    Task AddAsync(TEntity entity);
-
-    Task<TEntity> GetByIdAsync(Guid id);
-
     Task<List<TEntity>> GetAllAsync();
+
+    Task<TEntity?> GetByIdAsync(Guid id);
+
+    Task<IEnumerable<TEntity>> FindAsync(Expression<Func<TEntity, bool>> predicate);
+
+    Task AddAsync(TEntity entity);
 
     Task UpdateAsync(TEntity entity);
 
     Task RemoveAsync(Guid id);
-
-    Task<IEnumerable<TEntity>> FindAsync(Expression<Func<TEntity, bool>> predicate);
 
     Task<int> SaveChangesAsync();
 }
